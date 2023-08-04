a443099c1cb4d9a05b367c2111fb6adc074c1242
mat-daemon <mateuszsmigielski40@gmail.com>
2023-08-04 10:38:06 +0200

    Advisor basic funcionalities

--- advisor.cpp
+++ advisor.cpp
@@ -1,5 +1,27 @@
 #include <map>
+#include <iostream>
 #include "advisor.h"
+
+
+bool Advisor::opponent_in_range(const Individual* individual, const Individual* opponent){
+    int range = Individual::statistics.find(individual->type)->second.find("range")->second;
+
+    int distance_to_opponent = abs(opponent->x_coordinate - individual->x_coordinate) + abs(opponent->y_coordinate - individual->y_coordinate);
+
+    return range >= distance_to_opponent;
+}
+
+const Individual* Advisor::opponent_in_range(const Individual* individual, const std::vector<const Individual*>&  opponent_individuals){
+    for(const Individual* opponent : opponent_individuals){
+        if(opponent_in_range(individual, opponent)) return opponent;
+    }
+}
+
+std::pair<int, int> next_step(const Individual* individual, const Individual* target){
+    
+
+}
+
 
 
 std::vector<std::unique_ptr<Command>> Advisor::advise(){
@@ -9,37 +31,43 @@ std::vector<std::unique_ptr<Command>> Advisor::advise(){
     commands.push_back(std::make_unique<Attack>(50, 1, 2));
     commands.push_back(std::make_unique<Build>(70, 1, 'C'));
 
-    char player = 'P';
-    char opponent = 'E';
-
-    const Individual* player_base;
-    const Individual* opponent_base;
-
-    std::map<std::pair<int, int>, const Individual&> opponent_individuals;
-    std::vector<const Individual&> player_individuals;
 
     /*
-        Attack naive strategy
+        Naive attack strategy
         If player's individual can attack the base then attack.
         Attack advisor adds constant priority to every command as attack is the main objective.
         If opponent's individual is within the attack range, then check if one is stronger. When stronger then push to base else attack and fight.  
         Else push the base.
     */ 
 
-   for(const Individual& i : individuals){
+    char player = 'P';
+    char opponent = 'E';
+
+
+    // Split individuals
+    // It is safe using raw pointers, because individuals exists during program lifetime in the Reader
+    const Individual* player_base;
+    const Individual* opponent_base;
+    
+    std::vector<const Individual*>  opponent_individuals;
+    std::vector<const Individual*> player_individuals;
+
+
+    for(const Individual& i : individuals){
         if(i.owner == player){
-            if(i.type == 'B') player_base = &i;
-            else{
-                player_individuals.push_back(i);
-            }
+            if(i.type != 'B') player_individuals.push_back(&i);
+            else player_base = &i;
         }
         else{
-            if(i.type == 'B') opponent_base = &i;
-            else{
-                
-            }
+                if(i.type != 'B') opponent_individuals.push_back(&i);
+                else opponent_base = &i;
         }
-   }
+    }
+
+    
+    for(const Individual* i : player_individuals){
+        
+    }
 
     return commands;
 }
--- advisor.h
+++ advisor.h
@@ -10,8 +10,11 @@
 class Advisor{
     public:
         Advisor(const std::vector<std::vector<char>>& r_game_map, const std::vector<Individual>& r_individuals) : game_map(r_game_map), individuals(r_individuals){}
-        std::vector<std::unique_ptr<Command>> advise();
+        virtual std::vector<std::unique_ptr<Command>> advise();
     private:
+        bool opponent_in_range(const Individual* individual, const Individual* opponent);
+        const Individual* opponent_in_range(const Individual* individual, const std::vector<const Individual*>&  opponent_individuals);
+        std::pair<int, int> next_step(const Individual* individual, const Individual* target); 
         const std::vector<std::vector<char>>& game_map; 
         const std::vector<Individual>& individuals;
 };
